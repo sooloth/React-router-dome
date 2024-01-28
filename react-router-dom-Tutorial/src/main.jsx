74cab1fcbc833c6f7483e63cfee740ad34e79bd9
sooloth <pangnuar@gmail.com>
2024-01-28 14:25:48 +0700

    first

---
+++ react-router-dom-Tutorial/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Root from './routes/root'
+// import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './error-page'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
